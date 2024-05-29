9f36476c25fb82a303179f05f0e574318ae9f977
shvass <akshayb@gmx.com>
2024-05-29 13:56:49 +0530

    added urosHandler component
    + urosElement for managing micro-ros callbacks
    + urosHandler class for initialization of threaded executor

---
+++ urosHandler/include/urosHandler.hpp
@@ -0,0 +1,51 @@
+
+#ifndef UROS_HANDLER_HPP
+#define UROS_HANDLER_HPP
+
+#include <string>
+#include <vector>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "urosElement.hpp"
+
+
+/**
+ * @brief urosHandler to manage micro ros initialization
+ * 
+ */
+class urosHandler {
+
+public:
+
+    /**
+     * @brief Construct a new uros Handler object
+     * 
+     * @param node_name ros2 node name
+     */
+    urosHandler(std::string node_name = "uros_node");
+
+    /**
+     * @brief create a rclc executor on a new thread   
+     * 
+     * @param elements vector of urosElement pointer to run on these threaded executor
+     * @param CPUID  cpuid to run executor thread 
+     */
+    void addThreadExecutor(std::vector<urosElement*> elements, int CPUID = PRO_CPU_NUM);
+
+
+private:
+
+
+    static void executorTask(void* param);
+
+    rcl_allocator_t allocator;
+    rclc_support_t support;
+    rmw_init_options_t* rmw_options;
+    rcl_node_t node;
+};
+
+
+
+#endif UROS_HANDLER_HPP
---
+++ urosHandler/urosHandler.cpp
@@ -0,0 +1,87 @@
+#include "urosHandler.hpp"
+
+
+#include <uros_network_interfaces.h>
+#include <rmw_microros/rmw_microros.h>
+#include <freertos/task.h>
+
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+
+
+bool isUrosNetworkInit = false;
+
+urosHandler::urosHandler(std::string nodeName){
+    
+    if(!isUrosNetworkInit) {
+        ESP_ERROR_CHECK(uros_network_interface_initialize());
+        isUrosNetworkInit = true;
+    };
+
+    allocator = rcl_get_default_allocator();
+
+	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+	RCCHECK(rcl_init_options_init(&init_options, allocator));
+
+	rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
+	
+
+    bool waitAutoDiscovery = true;
+    while (waitAutoDiscovery)
+    {   
+        rmw_ret_t ret = rmw_uros_discover_agent(rmw_options);
+        if(ret == RMW_RET_OK) waitAutoDiscovery = false;
+        else if(ret == RMW_RET_TIMEOUT) waitAutoDiscovery = true;
+        else vTaskDelete(NULL);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+    
+    rmw_uros_sync_session(1000);
+
+	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+
+	// create node
+	RCCHECK(rclc_node_init_default(&node, nodeName.c_str(), "", &support));
+
+}
+
+void urosHandler::addThreadExecutor(std::vector<urosElement*> elements, int CPUID){
+
+    std::vector<urosElement*>* elems = new std::vector<urosElement*>(elements);
+
+    elems->at(0)->node = &node;
+    elems->at(0)->support = &support;
+    elems->at(0)->alloc = &allocator;
+
+    TaskHandle_t handle;
+    xTaskCreatePinnedToCore(executorTask, "uros-exec", 4096, elems, 2, &handle, CPUID);
+}
+
+
+
+void urosHandler::executorTask(void *param){
+    std::vector<urosElement*>* elems = (std::vector<urosElement*>*)param;
+
+    rclc_support_t* support = elems->at(0)->support;
+    rcl_allocator_t* alloc = elems->at(0)->alloc;
+    rcl_node_t* node = elems->at(0)->node;
+
+    
+    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+	RCCHECK(rclc_executor_init(&executor, &support->context, 2, alloc));
+
+    for(auto* elem : *elems){
+        elem->support = support;
+        elem->exec = &executor;
+        elem->alloc = alloc;
+        elem->node = node;
+
+        elem->init();
+    } 
+    
+    rclc_executor_spin(&executor);
+    vTaskDelete(NULL);
+}
